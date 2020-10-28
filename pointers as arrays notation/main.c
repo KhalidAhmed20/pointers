23168adeba9210fcb7d8b41b04648f9d68b684c4
Khalid <62681917+KhalidAhmed20@users.noreply.github.com>
2020-10-28 21:05:31 +0200

    Update main.c

--- pointers as arrays notation/main.c
+++ pointers as arrays notation/main.c
@@ -7,23 +7,23 @@ int main()
     int *p_arr = A; /* set p_arr to point to array A */
     int i ; // counter
     int index ; // counter
-    /* output array b using array subscript notation */
+    /* output array A using array subscript notation */
     printf( "Array A printed with:\nArray subscript notation\n" );
-    /* loop through array b */
+    /* loop through array A */
     for(i = 0 ; i < array_size ; i++){
         printf( "A[ %d ] = %d\n", i,A[i] );
     }
-    /* output array b using array name and pointer/index notation */
+    /* output array A using array name and pointer/index notation */
     printf( "\nPointer/index notation where\n the pointer is the array name\n" );
     for(index = 0 ; index < array_size ; index++){
         printf( "*( A + %d ) = %d\n", index, *(A+index) );
     }
-    /* output array b using p_arr and array subscript notation */
+    /* output array A using p_arr and array subscript notation */
     printf( "\nPointer subscript notation\n" );
     for(i = 0 ; i < array_size ; i++){
         printf( "p_arr[ %d ] = %d\n", i,p_arr[i] );
     }
-    /* output array b using p_arr and pointer/index notation */
+    /* output array A using p_arr and pointer/index notation */
     printf( "\nPointer/index notation\n" );
     for(index = 0 ; index < array_size ; index++){
         printf( "*(p_arr + %d ) = %d\n", index, *(p_arr+index) );
