d5cf56443c86e2e9b586b90e117f1592695b6498
Khalid <62681917+KhalidAhmed20@users.noreply.github.com>
2020-11-08 01:18:10 +0200

    Add files via upload

---
+++ Pointers with Functions/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* prototypes of functions*/
+int add(int a,int b);
+int sub(int a,int b);
+int mul(int a, int b);
+int division(int a,int b);
+int operation(int (*ptr_func)(int,int),int a,int b);
+
+
+int main()
+{
+    int x = 10 , y = 5;
+    int result;
+    /* call add function using operation function */
+    result = operation(add,x,y);
+    printf("result of x + y = %d\n",result);
+
+    /* call mul function using operation function */
+    result = operation(mul,x,y);
+    printf("result of x * y = %d\n",result);
+
+    return 0;
+}
+
+
+int add(int a,int b){
+	return a+b;
+}
+
+int sub(int a,int b){
+	return a-b;
+}
+
+int mul(int a, int b){
+	return a*b;
+}
+
+int division(int a,int b){
+	return a/b;
+}
+
+int operation(int (*ptr_func)(int,int),int a,int b)
+{
+    return (*ptr_func)(a,b);
+}
