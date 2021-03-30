3baf6d133e87c4f8ff120a4a9c9864ae1d92fbef
Khalid <62681917+KhalidAhmed20@users.noreply.github.com>
2021-03-30 18:16:13 +0200

    Update main.c

--- Pointers with 2D Array/main.c
+++ Pointers with 2D Array/main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    char str1[] = { 'K', 'H', 'A', 'L', 'E', 'D' };
+        char str1[] = { 'K', 'H', 'A', 'L', 'E', 'D' };
 	char str2[] = "KHALED";
 	char *str3 = "KHALID";
 	char  *cPtr = str1;
@@ -11,11 +11,11 @@ int main()
 	short sArr[] = {1, 2, 3, 4, 5};
 	short sArr2D[][5] = { {1, 2, 3, 4, 5}, {6, 7, 8, 9, 10} };
 
-    short *sPtr1 = sArr;
+        short *sPtr1 = sArr;
 	short (*sPtr2)[5] = sArr2D;  // (Pointer to Array of 2D) [increment by 5 places]..
 	short *sPtr3[5];    // Array of Pointers of type short
 
-    printf("sizeof(str1) = %u\n", sizeof(str1));
+        printf("sizeof(str1) = %u\n", sizeof(str1));
 	printf("sizeof(str2) = %u\n", sizeof(str2));
 	printf("sizeof(str3) = %u\n", sizeof(str3));
 	printf("sizeof(cPtr) = %u\n", sizeof(cPtr));
